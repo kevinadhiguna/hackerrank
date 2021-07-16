c8f175b059d4067b7cd672289c03374a3c1f8fff
kevinadhiguna <43397636+kevinadhiguna@users.noreply.github.com>
2021-07-16 15:17:22 +0700

    feat: add how to reverse string in JavaScript
    Reverse string using built-in methods in JavaScript

---
+++ reverseString.js
@@ -0,0 +1,9 @@
+function reverseString(str){
+  let revString = 
+      str
+        .split(""); // ["s", "t", "r", "i", "n", "g"]
+        .reverse(); // ["g", "n", "i", "r", "t", "s"]
+        .join("");  // "gnirts"
+}
+
+reverseString("string");

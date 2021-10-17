5a9dbbc9d32aba4476e500a66a76fc58151889de
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-17 00:41:29 +0000

    feat(algorithm): add grading students program

---
+++ algorithms/10.grading-students.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+    inputString = inputString.split("\n");
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    // Write your code here
+    let rounded_scores = [];
+    for (let i = 0; i < grades.length; i++) {
+        grades[i] < 38 || grades[i] % 5 < 3
+            ? rounded_scores.push(grades[i])
+            : rounded_scores.push(Math.ceil(grades[i] / 5) * 5);
+    }
+    return rounded_scores;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
